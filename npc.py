56ddd460818655d7601ed7891637d8e7fa5be1d2
Gosa Beselob <grt01010@example.com>
2023-12-26 17:51:59 +0300

    11

--- npc.py
+++ npc.py
@@ -10,7 +10,7 @@ class Npc:
         self.radom = 0
         self.x = x
         self.y = y
-        self.a = 1
+        self.ck = 3
     
     def res(self, okHo, kamera):
         kbadratnob = py.Rect(self.kbadrat.x + kamera.x , self.kbadrat.y + kamera.y, se.IGROKANOBA, se.IGROKANOBA)
@@ -19,6 +19,13 @@ class Npc:
             self.saob.ris(okHo, kamera)
 
     def dbig(self, igrok):
+        a = igrok.kbadrat.x - self.kbadrat.x
+        b = igrok.kbadrat.y - self.kbadrat.y
+        c = ((a*a)+(b*b))**0.5
+        sint = a/c
+        cost = b/c
+        skx = sint*self.ck
+        sky = cost*self.ck
         if abs(igrok.kbadrat.x-self.kbadrat.x)<50 and abs(igrok.kbadrat.y-self.kbadrat.y)<50:
             self.radom = 1
         else:
@@ -27,10 +34,8 @@ class Npc:
         self.saob.kbadrat.x = self.kbadrat.x
         self.saob.kbadrat.y = self.kbadrat.y
         if self.radom == 0:
-            self.kbadrat.x=self.kbadrat.x+self.a
-        if self.kbadrat.x - self.x > 200:
-            self.a=self.a*-1
-        if self.x - self.kbadrat.x > 200:
-            self.a=self.a*-1
+            self.kbadrat.x=self.kbadrat.x+skx
+            self.kbadrat.y=self.kbadrat.y+sky
+        
         
 
